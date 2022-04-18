50bfc69c52a5f3fba79ab0e2a0a17bd0cbf02a0e
Lucas <79713436+LucasVieiraS@users.noreply.github.com>
2022-04-18 15:20:53 -0300

    changed proxyreq header to the auth bearer

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ const processResponse = (proxyRes, res, append) => {
 };
 
 const onProxyReq = (proxyReq, req, res, options) => {
-  proxyReq.setHeader('User-Agent', proxyReq.getHeader('proxy-override-user-agent') || DEFAULT_USERAGENT);
+  proxyReq.setHeader("Authorization", `Bearer ${process.env.TWITTER_KEY}`)
   if (REWRITE_ACCEPT_ENCODING) {
     proxyReq.setHeader('Accept-Encoding', 'gzip');
   }
